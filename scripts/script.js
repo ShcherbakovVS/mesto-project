fda4492121f08d7c92a6c3633ebe872b2834e44c
Vlad <tezon2016@gmail.com>
2023-04-23 16:39:51 +0300

    refactor: rename variables, add modifiers for popups

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 const template = document.querySelector('#template-card').content;
 const edit = document.querySelector('.profile__edit');
 const add = document.querySelector('.profile__add');
-const popupEdit = document.querySelector('.popup');
-const popupAdd = document.querySelectorAll('.popup')[1];
-const popupImage = document.querySelectorAll('.popup')[2];
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupAdd = document.querySelector('.popup_type_add');
+const popupImage = document.querySelector('.popup_type_image');
 const popupImageClose = popupImage.querySelector('.popup__close');
 const popupEditClose = popupEdit.querySelector('.popup__close');
 const formElementEdit = popupEdit.querySelector('.popup__container');
@@ -16,32 +16,6 @@ const jobInput = formElementEdit.querySelector('#description');
 const nameProf = document.querySelector('.profile__name');
 const jobProf = document.querySelector('.profile__description');
 const elements = document.querySelector('.elements');
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
 
 function openPopup(pop) {
     pop.classList.add('popup_opened');
@@ -65,7 +39,7 @@ function handleFormSubmitEdit(evt) {
     evt.preventDefault();
     nameProf.textContent = nameInput.value;
     jobProf.textContent = jobInput.value;
-    popupEdit.classList.remove('popup_opened');
+    closePopup(popupEdit);
 }
 
 formElementEdit.addEventListener('submit', handleFormSubmitEdit);
@@ -78,11 +52,12 @@ popupAddClose.addEventListener('click', () => {
     closePopup(popupAdd);
 });
 
-function addCard(img, name) {
+function createCard(img, name) {
     const cardElement = template.querySelector('.elements__element').cloneNode(true);
     const image = cardElement.querySelector('.elements__image');
     const title = cardElement.querySelector('.elements__title');
     image.src = img;
+    image.alt = name
     title.textContent = name;
     cardElement.querySelector('.elements__like').addEventListener('click', (evt => {
         evt.target.classList.toggle('elements__like_actived');
@@ -92,7 +67,9 @@ function addCard(img, name) {
     });
     image.addEventListener('click', () => {
         openPopup(popupImage);
-        popupImage.querySelector('.popup__image').src = image.src;
+        const bigImg = popupImage.querySelector('.popup__image');
+        bigImg.src = image.src;
+        bigImg.alt = title.textContent;
         popupImage.querySelector('.popup__title-place').textContent = title.textContent; 
     });
     return cardElement;
@@ -100,14 +77,16 @@ function addCard(img, name) {
 
 function handleFormSubmitAdd(evt) {
     evt.preventDefault();
-    elements.prepend(addCard(imgInputAdd.value, nameInputAdd.value));
-    popupAdd.classList.remove('popup_opened');
+    elements.prepend(createCard(imgInputAdd.value, nameInputAdd.value));
+    imgInputAdd.value = '';
+    nameInputAdd.value = '';
+    closePopup(popupAdd);
 }
 
 formElementAdd.addEventListener('submit', handleFormSubmitAdd);
 
 initialCards.forEach((card) => {
-    elements.append(addCard(card.link, card.name));
+    elements.append(createCard(card.link, card.name));
 })
 
 popupImageClose.addEventListener('click', () => {
